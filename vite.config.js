c7ac3bae44508f523b64f2899fee2574f67f5cb8
Sendygk <sendygk@gmail.com>
2026-05-10 11:24:41 +0700

    Configure deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/qna-top-3_sendy/",
   plugins: [
     react(),
     tailwindcss(),
